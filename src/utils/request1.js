7c5c974f72521b03f2d3376b3638a5dc750d5488
FE-lzy <112347674@qq.com>
2019-11-11 22:47:45 +0800

    5

--- src/utils/request1.js
+++ src/utils/request1.js
@@ -23,7 +23,7 @@ service.interceptors.request.use(
       // let each request carry token
       // ['X-Token'] is a custom headers key
       // please modify it according to the actual situation
-      config.headers['authorization'] = getToken()
+      config.headers['authorization'] = "Bearer "+getToken()
     }
     return config
   },
